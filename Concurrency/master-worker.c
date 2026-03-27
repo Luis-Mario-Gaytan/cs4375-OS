053e93c2e80e2258dbd511f702455ffdca0c64d2
Luis-Mario-Gaytan <lgaytanguz@miners.utep.edu>
2026-03-27 01:07:46 -0600

    Assignment-3 END

--- Concurrency/master-worker.c
+++ Concurrency/master-worker.c
@@ -29,12 +29,13 @@ pthread_cond_t full =
 pthread_cond_t empty =
     PTHREAD_COND_INITIALIZER; // Signaled when buffer has data
 
+int in = 0, out = 0, count = 0;
 int produced_count = 0; // total number produced so far (0...M)
 int consumed_count = 0; // total number consumed so far (0...M)
-
-int item_to_produce, curr_buf_size;
-int total_items, max_buf_size, num_workers, num_masters;
-
+int M, N, C, P;         // M(total number to produce)
+                        // N(buffer size)
+                        // C(number of consumer)
+                        // P(number of producer)
 int *buffer;
 
 /*
@@ -44,9 +45,37 @@ int *buffer;
  * Signal consumers that buffer is not empty
  * Unlock mutex
  */
-
 void *producer(void *arg) {
-  // TODO implement producer function.
+  int id = *(int *)arg;
+  free(arg);
+  while (1) {
+    pthread_mutex_lock(&mutex);
+
+    // Wait if buffer is full
+    while (count == N) {
+      pthread_cond_wait(&full, &mutex);
+    }
+    // if all numbers produced, exit
+    if (produced_count >= M) {
+      pthread_mutex_unlock(&mutex);
+      break;
+    }
+
+    // produce next number
+    int num = produced_count;
+    produced_count++;
+
+    // insert into buffer
+    buffer[in] = num;
+    in = (in + 1) % N;
+    count++;
+
+    printf("Producer %d produced %d\n", id, num);
+
+    // Signal that buffer is not empty
+    pthread_cond_signal(&empty);
+    pthread_mutex_unlock(&mutex);
+  }
   return NULL;
 }
 
@@ -60,88 +89,99 @@ void *producer(void *arg) {
  * Unlock mutex
  */
 void *consumer(void *arg) {
-  // TODO implement consumer function
-  return NULL;
-}
-void print_produced(int num, int master) {
-
-  printf("Produced %d by master %d\n", num, master);
-}
-
-void print_consumed(int num, int worker) {
-
-  printf("Consumed %d by worker %d\n", num, worker);
-}
-
-// produce items and place in buffer
-// modify code below to synchronize correctly
-void *generate_requests_loop(void *data) {
-  int thread_id = *((int *)data);
-
+  int id = *(int *)arg;
   while (1) {
+    pthread_mutex_lock(&mutex);
 
-    if (item_to_produce >= total_items) {
+    // Wait until buffer has data, but also check if done.
+    while (count == 0 && consumed_count < M) {
+      pthread_cond_wait(&empty, &mutex);
+    }
+
+    // if all number have been consumed, exit
+    if (consumed_count >= M) {
+      pthread_mutex_unlock(&mutex);
       break;
     }
 
-    buffer[curr_buf_size++] = item_to_produce;
-    print_produced(item_to_produce, thread_id);
-    item_to_produce++;
-  }
-  return 0;
-}
+    // consume the next number
+    int num = buffer[out];
+    out = (out + 1) % N;
+    count--;
+    consumed_count++;
 
-// write function to be run by worker threads
-// ensure that the workers call the function print_consumed when they consume an
-// item
+    printf("Consumer %d consumed %d\n", id, num);
+
+    // signal that buffer is not full
+    pthread_cond_signal(&full);
+    pthread_mutex_unlock(&mutex);
+  }
+  return NULL;
+}
 // TODO Update main function
 // Create both producer and consumer threads (P producer, C consumer)
 // Join all threads after they finish
 // Free resources
 
 int main(int argc, char *argv[]) {
-  int *master_thread_id;
-  pthread_t *master_thread;
-  item_to_produce = 0;
-  curr_buf_size = 0;
+  // Parse command-line arguments
+  if (argc != 5) {
+    fprintf(stderr,
+            "Usage: %s, M (Total number) N (Maximum shared buffer size) C "
+            "(Number of consumers) P (number of producers)\n",
+            argv[0]);
+    exit(EXIT_FAILURE);
+  }
+  M = atoi(argv[1]);
+  N = atoi(argv[2]);
+  C = atoi(argv[3]);
+  P = atoi(argv[4]);
 
-  int i;
-
-  if (argc < 5) {
-    printf("./master-worker #total_items #max_buf_size #num_workers #masters "
-           "e.g. ./exe 10000 1000 4 3\n");
-    exit(1);
-  } else {
-    num_masters = atoi(argv[4]);
-    num_workers = atoi(argv[3]);
-    total_items = atoi(argv[1]);
-    max_buf_size = atoi(argv[2]);
+  if (M <= 0 || N <= 0 || C <= 0 || P <= 0) {
+    fprintf(stderr, "All arguments must be positive integers.\n");
+    exit(EXIT_FAILURE);
   }
 
-  buffer = (int *)malloc(sizeof(int) * max_buf_size);
-
-  // create master producer threads
-  master_thread_id = (int *)malloc(sizeof(int) * num_masters);
-  master_thread = (pthread_t *)malloc(sizeof(pthread_t) * num_masters);
-  for (i = 0; i < num_masters; i++)
-    master_thread_id[i] = i;
-
-  for (i = 0; i < num_masters; i++)
-    pthread_create(&master_thread[i], NULL, generate_requests_loop,
-                   (void *)&master_thread_id[i]);
-
-  // create worker consumer threads
-
-  // wait for all threads to complete
-  for (i = 0; i < num_masters; i++) {
-    pthread_join(master_thread[i], NULL);
-    printf("master %d joined\n", i);
+  // Allocate buffer
+  buffer = (int *)malloc(N * sizeof(int));
+  if (buffer == NULL) {
+    perror("malloc");
+    exit(EXIT_FAILURE);
   }
 
-  /*----Deallocating Buffers---------------------*/
+  // Create producer threads
+  pthread_t producers[P];
+  for (int i = 0; i < P; i++) {
+    int *id = (int *)malloc(sizeof(int));
+    *id = i;
+    if (pthread_create(&producers[i], NULL, producer, id) != 0) {
+      perror("pthread_create");
+      exit(EXIT_FAILURE);
+    }
+  }
+
+  // Create consumer threads
+  pthread_t consumers[C];
+  for (int i = 0; i < C; i++) {
+    int *id = (int *)malloc(sizeof(int));
+    *id = i;
+    if (pthread_create(&consumers[i], NULL, consumer, id) != 0) {
+      perror("pthread_create");
+      exit(EXIT_FAILURE);
+    }
+  }
+
+  // Wait for all producers to finish
+  for (int i = 0; i < P; i++) {
+    pthread_join(producers[i], NULL);
+  }
+
+  // Wait for all consumers to finish
+  for (int i = 0; i < C; i++) {
+    pthread_join(consumers[i], NULL);
+  }
+
   free(buffer);
-  free(master_thread_id);
-  free(master_thread);
-
+  printf("All producers finished. Exiting.\n");
   return 0;
 }
